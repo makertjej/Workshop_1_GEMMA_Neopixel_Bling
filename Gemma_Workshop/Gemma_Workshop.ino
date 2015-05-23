3953682680521bca38fc6a9e23898743775cbf39
Jessica <jcnayes@Jessicas-MacBook-Pro-new.local>
2015-05-23 16:19:48 +0200

    Ready to publish!
    Added title comments, more comments throughout, merged with version
    from older workshop description, fixed bugs.

--- Gemma_Workshop/Gemma_Workshop.ino
+++ Gemma_Workshop/Gemma_Workshop.ino
@@ -1,40 +1,51 @@
-// FULL THING
-// Low power NeoPixel earrings example.  Makes a nice blinky display
-// with just a few LEDs on at any time...uses MUCH less juice than
-// rainbow display!
+// MAKERTJEJ.se
+// Workshop 1: Gemma + NeopPixel Ring
+//
+// Low power program for GEMMA with a NeoPixel ring. 
+// A nice blinky display that can be personalized by 
+// choosing different colors, speeds & patterns.
 
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 0 // assign constant PIN to the pin D0~ on the Gemma
 
-// User-defined parameters
-#define myPattern 0
-#define myColorChart 3
-#define mySpeed 2
-
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(16, PIN);
 
 
-// MODIFYING VARIABLES
+//**********************************************************************************************
+// MODIFIABLE PARAMETERS -- PERSONALIZE YOUR GEMMA NEOPIXELS HERE
+//**********************************************************************************************
 //
-// BLINKING PATTERNS
-// CHOOSE BLINKING PATTERN HERE: 0-3 - default blinking pattern is random
-//
+// CHOOSE BLINKING PATTERN
+// 
+// 0: RANDOM BLINKING
+// 1: COLOR FLUSH
+// 2: SPINNING
 
-// uint8_t myPattern = 0; // Current animation effect
+uint8_t  myPattern = 0; // ← Change this value to your desired pattern
 
-// COLOR CHOICES
-// CHOOSE COLOR PALETTE HERE: 0-5 - default color chart is rainbow
-// 0: REDS-ORANGE; 1: BLUE-LILAC; 2: GREEN-YELLOW; 3: PINK-PURPLE; 4: TURQOISE; 5: RAINBOW
+// CHOOSE COLOR PALETTE HERE
+// 
+// 0: REDS-ORANGE
+// 1: BLUE-LILAC
+// 2: GREEN-YELLOW
+// 3: PINK-PURPLE
+// 4: TURQUOISE
+// 5: RAINBOW
 
-// uint8_t myColorChart = 3;
+uint8_t myColorChart = 5; // ← Change this value to your desired color palette
 
-// TIMING CHOICES
 // CHOOSE A SPEED SETTING HERE
-// 0: SLOW; 1: MEDIUM; 2: FAST -- DEFAULT SETTING IS FAST
+//
+// 0: FAST
+// 1: MEDIUM
+// 2: SLOW 
 
-// uint8_t mySpeed = 2;
+uint8_t mySpeed = 0; // ← Change this value to your desired speed
+
+//**********************************************************************************************
+// YOU’RE DONE!! YOU CAN LOOK AT THE REST OF THE CODE LATER
+//**********************************************************************************************
 
 // UTILITY VARIABLES
 uint32_t prevTime; // Utility variable for timing
@@ -43,6 +54,7 @@ uint32_t color = 0xffffff; //default white
 uint8_t offset = 0; // Current position of moving pattern
 uint8_t c_offset = 0; // Current position within colour chart
 
+// Colors, hexadecimal code:
 uint32_t myColors[][6] = {{0xff0000, 0xff3300, 0xff6600, 0xffcc00, 0xffff00}, // reds-orange
 {0x0000ff, 0x3300ff, 0x6600ff, 0x9900ff, 0xcc00ff, 0xff00ff}, // blue-lilac
 {0x00ff00, 0x66ff00, 0xccff00, 0xffff00, 0xffee00, 0xffdd00}, // green-yellow
@@ -52,64 +64,64 @@ uint32_t myColors[][6] = {{0xff0000, 0xff3300, 0xff6600, 0xffcc00, 0xffff00}, //
 };
 
 
-void setup() {
+void setup() { // setup() runs once
   pixels.begin();
   pixels.setBrightness(60); // 1/3 brightness to save battery
   prevTime = millis();
 
-    switch (mySpeed) {
-      case 0:
-          delayTime = 500;
+    switch (mySpeed) { // where the speed timings are set
+      case 0: // speed 0
+          delayTime = 50;
       break;
 
-      case 1:
+      case 1: // speed 1
+          delayTime = 150;
+      break;
+
+      default: // speed 2 (or any integer other than 0 or 1)
           delayTime = 300;
-      break;
-
-      default:
-          delayTime = 100;
       break;
   }
 }
 
-void loop() {
-  uint8_t  i;
+void loop() { // after setup() runs, loop() will run over and over as long as the GEMMA has power
+  uint8_t  i, j;
   uint32_t t;
-
 
   switch(myPattern) {
 
-    case 0: // Random sparks - just one LED on at a time!
-      i = random(16);
-      color = myColors[myColorChart][random(6)];
-      pixels.setPixelColor(i, color);
-      pixels.show();
-      delay(delayTime);
-      pixels.setPixelColor(i, 0);
-      break;
+    case 0: // Random sparks - just one-three LED(s) on at a time!
+    
+  	i = random(16);
+  	for(uint8_t i=0; i<4; i++) {
+      	  color = myColors[myColorChart][random(6)];
+      	  j = random(16);
+      	  pixels.setPixelColor(j, color);
+  	}
+  	pixels.show();
+  	delay(delayTime+50);
+  	for(uint16_t i=0; i<16; i++) {
+    	  pixels.setPixelColor(i, 0);
+  	}
+  	break;
  
     case 1: // Flushes the ring with one color at a time
 
-      for(uint16_t i=0; i<16; i++) {
-        color = 0;
-        if(i>=offset) color=myColors[myColorChart][c_offset];
+  	for(uint16_t i=0; i<16; i++) {
+  	  color = myColors[myColorChart][c_offset];
+    	  pixels.setPixelColor(i, color);
+      	  pixels.show();
+      	  delay(delayTime);
+  	}
     
-        pixels.setPixelColor(i, color);
-        pixels.show();
-        delay(delayTime);
-
-      }
-    
-      offset++;
-      if (offset>=16) {
-        offset=0;
-        c_offset++;
-        if (c_offset>5) c_offset=0;
-        for (uint16_t i=0; i<16; i++) {
-          pixels.setPixelColor(i, 0);
-        }
-      }
-      break;
+  	c_offset++;
+  	if (c_offset>=6) {
+    	  c_offset=0;
+  	}
+  	for (uint16_t i=0; i<116; i++) {
+    	  pixels.setPixelColor(i, 0);
+  	}
+  	break;
 
     case 2: // Spinny wheels (8 LEDs on at a time)
        for(i=0; i<16; i++) {
